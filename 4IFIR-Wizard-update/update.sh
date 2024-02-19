95b839e37549a857f314114276edc506a6f6c904
postcanonical <postcanonical@tuta.io>
2024-02-19 15:57:47 +0200

    release script

--- 4IFIR-Wizard-update/update.sh
+++ 4IFIR-Wizard-update/update.sh
@@ -1,6 +1,8 @@
-rm -v 4IFIR-Wizard-update/switch/.packages/4IFIR\ Wizard/files.zip
+new_version=$(date "+%y.%m.%d.%H")
+sed -i "s/;version='[0-9.]*'/;version='$new_version'/g" 4ifir\ Wizard\ source\ code/config.ini
+rm -v 4IFIR-Wizard-update/4IFIR\ Wizard/files.zip
 cd 4ifir\ Wizard\ source\ code/
-zip -r ../4IFIR-Wizard-update/switch/.packages/4IFIR\ Wizard/files.zip ./*
+zip -r ../4IFIR-Wizard-update/4IFIR\ Wizard/files.zip ./*
 cd ../4IFIR-Wizard-update
-rm -v "4IFIR-Wizard-update-$(date +"%d.%m.%y-%H").zip"
-zip -r "4IFIR-Wizard-update-$(date +"%d.%m.%y-%H").zip" ./switch
+rm -v "4IFIR-Wizard-update-$new_version.zip"
+zip -r "4IFIR-Wizard-update-$new_version.zip" ./4IFIR\ Wizard
