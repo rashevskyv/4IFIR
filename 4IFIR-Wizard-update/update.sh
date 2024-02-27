c333bd1c0afbb9578caa4c6da31816f5e387dc21
postcanonical@tuta.io <37911092+postcanonical@users.noreply.github.com>
2024-02-27 13:14:23 +0000

    Delete 4IFIR-Wizard-update/update.sh

--- 4IFIR-Wizard-update/update.sh
+++
@@ -1,8 +0,0 @@
-new_version=$(date "+%y.%m.%d.%H")
-sed -i "s/;version='[0-9.]*'/;version='$new_version'/g" 4ifir\ Wizard\ source\ code/config.ini
-rm -v 4IFIR-Wizard-update/4IFIR\ Wizard/files.zip
-cd 4ifir\ Wizard\ source\ code/
-zip -r ../4IFIR-Wizard-update/4IFIR\ Wizard/files.zip ./*
-cd ../4IFIR-Wizard-update
-rm -v "4IFIR-Wizard-update-$new_version.zip"
-zip -r "4IFIR-Wizard-update-$new_version.zip" ./4IFIR\ Wizard
